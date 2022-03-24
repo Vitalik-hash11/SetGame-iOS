569ecd86feefc167409bdabc7d0e817179954886
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-03-24 22:33:14 +0200

    implement basic ui, add constraints, add outlets and action

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cardList: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func cardButtonPressed(_ sender: UIButton) {
+        //TODO: handle card selection
+    }
+    
 
 }
 
